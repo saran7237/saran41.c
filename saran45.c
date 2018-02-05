f8b97b06323735e01c87a37f97a1228a52a054d2
saran7237 <36067371+saran7237@users.noreply.github.com>
2018-02-05 16:18:21 +0530

    Create saran45.c

---
+++ saran45.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int b;
+printf("enter the year");
+scanf("%d",&b);
+if(b%4==0)
+{
+printf("ITS A LEAP YEAR");
+}
+else
+{
+printf("SORRY......ITS NOT A LEAP YEAR");
+}
+return 0;
+}
